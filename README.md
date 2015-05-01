f6b13b55b75b87452c7ea7121561f55a229e0c99
Kris Leech <kris.leech@gmail.com>
2015-05-01 19:35:57 +0100

    Component with state editor Deploy to Divshot

---
+++ README.md
@@ -0,0 +1,12 @@
+# Visualize
+
+[http://development.visualize.divshot.io](http://development.visualize.divshot.io)
+
+Experiments with ClojureScript interactivity.
+
+## Development
+
+```
+lein figsheel
+open http://localhost:3449
+```
--- src/cljs/visualize/core.cljs
+++ src/cljs/visualize/core.cljs
@@ -7,12 +7,29 @@
               [cljsjs.react :as react])
     (:import goog.History))
 
+(defonce world (atom { :todos [] }))
+
 ;; -------------------------
 ;; Views
 
+(def done-to-color { true "green"
+                     false nil })
+
+(defn todo []
+  (let [state (reagent/atom { :done false :name "Buy milk" })]
+    (fn [] 
+      [:div.todo
+      [:input.done { :type "checkbox" :done (:done @state) :on-click #(swap! state assoc :done (not (:done @state)))}]
+      [:input.name { :type "text" :style { :border "none" :color (done-to-color (:done @state)) } :value (:name @state) :on-change #(swap! state assoc :name (-> % .-target .-value)) }]
+      [:br]
+      [:input.state-editor { :type "text" :size 55 :value (str @state) :on-change #(reset! state (cljs.reader/read-string (-> % .-target .-value))) }]])))
+
 (defn home-page []
   [:div [:h2 "Welcome to visualize"]
+   [todo]
+   [:p "Try changing the data or using the compontent"]
    [:div [:a {:href "#/about"} "go to about page"]]])
+
 
 (defn about-page []
   [:div [:h2 "About visualize"]
